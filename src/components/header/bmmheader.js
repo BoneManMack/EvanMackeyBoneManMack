d9581216263b3c4174f7f75ef3cf93b41653bed3
Evan Mackey <Evan.Stewart.Mackey@gmail.com>
2021-06-22 16:22:34 -0700

    Added header js and css

---
+++ src/components/header/bmmheader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './bmmheader.css';
+
+function BMMheader() {
+    return(
+        <div>
+            <div className="titleCard">
+                <h1>Evan Mackey</h1>
+                <h2>Bone Man Mack</h2>
+                <h3>Los Angeles Trombonist</h3>
+            </div>
+        </div>
+    )
+}
+
+export default BMMheader;
