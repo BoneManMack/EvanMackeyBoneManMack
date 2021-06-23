335acf83f87460a7cda7f1a46fff42b68f490a8c
Evan Mackey <Evan.Stewart.Mackey@gmail.com>
2021-06-22 18:59:41 -0700

    Added error page with css, added error page to main display, added CSS to improve readablity of headers

--- src/components/maindisplay/bmmmaindisplay.js
+++ src/components/maindisplay/bmmmaindisplay.js
@@ -5,27 +5,28 @@ import './bmmmaindisplay.css';
 
 import BMMhome from './home/bmmhome';
 
+import BMMerror from './error/bmmerror';
+
 function BMMmaindisplay() {
     return(
         <div>
             <main>
                 <Switch>
                     <Route path="/" exact component={BMMhome} />
-                    
+                    <Route path="*" component={BMMerror} />
                 </Switch>
             </main>
         </div>
     )
 }
 
-/* HOLDING AREA FOR FUTURE COMPONENTS
+/* HOLDING AREA FOR FUTURE COMPONENTS---------------------------------------------
 <Route path="/performance_and_recording" component={BMMpandr} />
                     <Route path="/writing_and_arranging" component={BMMwanda} />
                     <Route path="/teaching" component={BMMteach} />
                     <Route path="/blog" component={BMMblog} />
                     <Route path="/links" component={BMMlinks} />
                     <Route path="/contact" component={BMMcontact} />
-                    <Route component={BMMerror} />
-*/
+--------------------------------------------------------------------------------*/
 
 export default BMMmaindisplay
---
+++ src/components/maindisplay/error/bmmerror.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './bmmerror.css';
+
+function BMMerror() {
+    return(
+        <div className="error">
+            <h3>We're sorry the page you've attempted to reach doesn't exist.</h3> 
+            <h4>Please click one of the links in the navbar to get to an existing page.</h4>
+        </div>
+    )
+}
+
+export default BMMerror;
