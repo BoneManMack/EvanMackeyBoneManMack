37cb89214dfeb215ab622d5e044aaf5b7845c58f
Evan Mackey <Evan.Stewart.Mackey@gmail.com>
2021-06-22 17:07:34 -0700

    added header component to main component

--- src/BoneManMack.js
+++ src/BoneManMack.js
@@ -1,9 +1,11 @@
 import './BoneManMack.css';
 
+import BMMheader from './components/header/bmmheader';
+
 function BoneManMack() {
   return (
     <div className="App">
-
+      <BMMheader />
     </div>
   );
 }
