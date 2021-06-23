3b250aef91e043185f2b9f94337031ad128203d8
Evan Mackey <Evan.Stewart.Mackey@gmail.com>
2021-06-22 19:05:55 -0700

    Added under construction segment for the blog page, added blog route to main display

---
+++ src/components/maindisplay/blog/bmmblog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BMMblog() {
+    return(
+        <h2 style={{fontSize: 50 + "px", textAlign: "center", margin: 8 + "rem"}}>Under Construction come back soon</h2>
+    )
+}
+
+export default BMMblog;
--- src/components/maindisplay/bmmmaindisplay.js
+++ src/components/maindisplay/bmmmaindisplay.js
@@ -5,6 +5,8 @@ import './bmmmaindisplay.css';
 
 import BMMhome from './home/bmmhome';
 
+import BMMblog from './blog/bmmblog';
+
 import BMMerror from './error/bmmerror';
 
 function BMMmaindisplay() {
@@ -13,6 +15,7 @@ function BMMmaindisplay() {
             <main>
                 <Switch>
                     <Route path="/" exact component={BMMhome} />
+                    <Route path="/blog" component={BMMblog} />
                     <Route path="*" component={BMMerror} />
                 </Switch>
             </main>
@@ -20,13 +23,12 @@ function BMMmaindisplay() {
     )
 }
 
-/* HOLDING AREA FOR FUTURE COMPONENTS---------------------------------------------
-<Route path="/performance_and_recording" component={BMMpandr} />
+/* HOLDING AREA FOR FUTURE COMPONENTS-----------------------------------------------
+                    <Route path="/performance_and_recording" component={BMMpandr} />
                     <Route path="/writing_and_arranging" component={BMMwanda} />
                     <Route path="/teaching" component={BMMteach} />
-                    <Route path="/blog" component={BMMblog} />
                     <Route path="/links" component={BMMlinks} />
                     <Route path="/contact" component={BMMcontact} />
---------------------------------------------------------------------------------*/
+----------------------------------------------------------------------------------*/
 
 export default BMMmaindisplay
